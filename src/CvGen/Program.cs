bbe3333b13f8362465db68c5c071bc7c60ed1bd0
Oliver <oliver.g.sk@gmail.com>
2026-07-12 02:21:03 +0200

    Reorg

---
+++ src/CvGen/Program.cs
@@ -0,0 +1,19 @@
+﻿using RazorLight;
+
+var engine = new RazorLightEngineBuilder()
+    .UseFileSystemProject("Templates")
+    .UseMemoryCachingProvider()
+    .Build();
+
+var html = await engine.CompileRenderAsync(
+    "index.cshtml",
+    new { });
+
+string path = Path.Combine("../docs");
+Directory.CreateDirectory("../docs");
+
+await File.WriteAllTextAsync(
+    "../docs/index.html",
+    html);
+
+Console.WriteLine("Generated docs/index.html");
---
+++ src/CvGen/Templates/index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CvGen.Templates;
+
+public class index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
